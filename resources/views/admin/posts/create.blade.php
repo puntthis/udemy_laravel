b4fa0a7ce5295ec895a0b51b43e4574ea181a855
Jonathan Lawhorn <jonathan.lawhorn@bluetangerinesolutions.com>
2018-02-24 21:37:53 -0500

    Able to create post with static category

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Post</h1>
+
+    {!! Form::open(['method' => 'POST', 'action' => 'AdminPostsController@store', 'files' => true]) !!}
+    <div class="form-group">
+        {!! Form::label('title', 'Title') !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('category_id', 'Category') !!}
+        {!! Form::select('category_id', [0 => 'options'], null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('photo_id', 'Photo') !!}
+        {!! Form::file('photo_id', ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('body', 'Description') !!}
+        {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Create Post', ['class' => 'btn btn-primary']) !!}
+    </div>
+    {!! Form::close() !!}
+
+    @include('includes.form_error')
+@stop
